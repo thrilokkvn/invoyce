d0ab8b5178613253b291d67add9720aa7d3255e7
thrilokkvn <thriloknath15vemula@gmail.com>
2025-08-13 21:11:40 +0530

    feat: added next auth and prisma

---
+++ src/app/login/page.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { signIn } from "@/config/auth";
+
+export default function Login() {
+    return (
+        <div className="flex h-screen w-full items-center justify-center px-4">
+            <Card className="w-sm">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardDescription>Enter your email below to login to your account</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form
+                        action={async () => {
+                            "use server"
+                            await signIn()
+                        }}
+                        className="flex flex-col gap-y-4"
+                    >
+                        <div className="flex flex-col gap-y-2">
+                            <Label>Email</Label>
+                            <Input placeholder="john@example.com"/>
+                        </div>
+                        <Button>Submit</Button>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
---
+++ src/config/auth.ts
@@ -0,0 +1,21 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth"
+import Nodemailer from "next-auth/providers/nodemailer"
+import prisma from "./prisma"
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Nodemailer({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+})
